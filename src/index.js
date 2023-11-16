4d388b766f447060f7e926a22960ebb3489f7757
parkjian7 <masichyun7@inswave.com>
2023-11-16 23:59:30 +0900

    header 기본뼈대 제작

--- src/index.js
+++ src/index.js
@@ -2,16 +2,40 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import NotFound from './pages/NotFound';
+import AllProducts from './pages/AllProducts';
+import NewProduct from './pages/NewProduct';
+import ProductDetail from './pages/ProductDetail';
+import MyCart from './pages/MyCart';
+import Home from './pages/Home';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Support from './pages/Support';
+import MyPage from './pages/MyPage';
+import Signup from './pages/Signup';
+import Signin from './pages/Signin';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      {index: true, path: '/', element: <Home />},
+      {path: 'products', element: <AllProducts />},
+      {path: 'products/new', element: <NewProduct/>},
+      {path: 'products/:id', element: <ProductDetail/>},
+      {path: 'signin', element: <Signin/>},
+      {path: 'signup', element: <Signup/>},
+      {path: 'mypage', element: <MyPage/>},
+      {path: 'carts', element: <MyCart/>},
+      {path: 'support', element: <Support/>}
+    ]
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
