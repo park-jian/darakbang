1553dad8935840dc71ebaf43e1b2c57da215e204
parkjian7 <masichyun7@inswave.com>
2023-12-12 22:31:13 +0900

    기본 login구현

--- src/pages/AllProducts.jsx
+++ src/pages/AllProducts.jsx
@@ -1,5 +1,12 @@
 import React from "react";
+import Header from "../components/Header";
+import Navbar from "../components/Navbar";
 
 export default function AllProducts() {
-  return <div>AllProducts</div>;
+  return (
+    <>
+      <Header />
+      <Navbar />
+    </>
+  );
 }
