af49bd309568f9b3e0d4c90c6e30f1ae7e2e485b
parkjian7 <masichyun7@inswave.com>
2023-12-20 00:21:32 +0900

    메인페이지 구현 및 상품 클릭시 상세 페이지1

---
+++ src/components/product/Product.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+export default function Product({
+  alt,
+  item,
+  item: { id, name, soldout, size, color, price, quantity, thumbnails },
+}) {
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate(`/products/${id}`, { state: { item } });
+  };
+  return (
+    <li
+      onClick={handleClick}
+      className="rounded-lg shadow-md overflow-hidden cursor-pointer transition-all hover:scale-105"
+    >
+      <img className="w-full" id={id} src={thumbnails.default.url} alt={alt} />
+      <div className="mt-2 px-2 text-lg flex justify-between items-center">
+        <p>{`${name}`}</p>
+        <p>{`${quantity}개`}</p>
+        <p>{`1박 ${price}원`}</p>
+      </div>
+    </li>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import itemList from "../data/itemList.json";
+import Product from "../components/product/Product";
 
 export default function Home() {
-  return <div>Home</div>;
+  const [product, setProduct] = useState([]);
+  // const fetchProductImages = async () => {
+  //   try {
+  //     // 상품 이미지 목록을 가져오는 API 호출 또는 이미지 URL 배열을 가져오는 로직
+  //     const response = await fetch('URL_상품_이미지_목록');
+  //     const data = await response.json(); // JSON 형태로 받아온다고 가정
+
+  //     // 받아온 데이터에서 이미지 URL들을 추출하여 설정
+  //     const imageUrls = data.map(item => item.imageUrl);
+  //     setProductImages(imageUrls);
+  //   } catch (error) {
+  //     console.error('상품 이미지를 가져오는 중 오류가 발생했습니다.', error);
+  //   }
+  // };
+  const extractItem = (items) => {
+    // JSON 파일에서 이미지 URL 추출
+    const item = items.map((item) => {
+      return item;
+    });
+    setProduct(item);
+  };
+
+  useEffect(() => {
+    // 컴포넌트가 마운트될 때 상품 이미지를 가져오는 함수 호출
+    extractItem(itemList.items);
+  }, []); // 빈 배열을 전달하여 한 번만 호출되도록 설정
+  return (
+    <div className="bg-slate-200">
+      <h1>실시간 BEST</h1>
+      <ul className="flex">
+        <li>일간 BEST</li>
+        <li>주간 BEST</li>
+        <li>월간 BEST</li>
+      </ul>
+      <div className="grid grid-cols-1 md:grid-cols-3 lg-grid-cols-4 gap-4 p-4">
+        {product.map((item, index) => (
+          <Product key={item.id} alt={`상품 이미지 ${index + 1}`} item={item} />
+        ))}
+      </div>
+    </div>
+  );
 }
