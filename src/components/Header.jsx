61f5cfb98e1032515b895db8be9b384293367d92
parkjian7 <masichyun7@inswave.com>
2023-12-21 00:35:27 +0900

    Header추가

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,12 @@
 import React from "react";
-import banner2 from "../image/banner2.jpg";
+import hotel from "../image/hotel.jpg";
+import Navbar from "./Navbar";
 
 export default function Header() {
-  return <>{/* <img src={banner2} alt="Banner" /> */}</>;
+  return (
+    <>
+      <img className="max-h-96 w-full" src={hotel} alt="Banner" />
+      <Navbar />
+    </>
+  );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,36 +4,42 @@ import { FiAlignJustify } from "react-icons/fi";
 import { RiShoppingCart2Line } from "react-icons/ri";
 import { GiCharacter } from "react-icons/gi";
 import { FaSearch } from "react-icons/fa";
-import star from "../image/star.gif";
+import home from "../image/home.png";
 
 export default function Navbar() {
   return (
     <div className="flex flex-row h-40 bg-blue-200 items-center">
-      <button className="w-30 text-3xl flex flex-col items-center justify-center h-full bg-sky-500 text-white">
+      <button className="w-30 text-base flex flex-col items-center justify-center h-full bg-sky-500 text-white">
         <FiAlignJustify />
         카테고리
       </button>
-      <div className="h-full">
-        <div className="flex flex-row h-36 items-center">
-          <Link to="/">
-            <img src={star} alt="logo" />
+      <div className="h-full w-full p-3">
+        <div className="flex flex-row h-20 items-center">
+          <Link to="/" className="mr-6">
+            <img src={home} alt="logo" />
           </Link>
-          <div className="flex items-center border-2 rounded-lg border-black w-60">
+          <div className="flex items-center border-2 rounded-lg border-black w-60 mr-6">
             <input className="border-0 bg-transparent w-60" type="text" />
             <button className="right-0">
               <FaSearch />
             </button>
           </div>
-          <Link to="mypage" className="">
-            <GiCharacter />
+          <Link
+            to="mypage"
+            className="mr-6 flex justify-center items-center flex-col"
+          >
+            <GiCharacter className="" />
             <p>마이페이지</p>
           </Link>
-          <Link to="carts">
+          <Link
+            to="carts"
+            className="mr-6 flex justify-center items-center flex-col"
+          >
             <RiShoppingCart2Line />
-            장바구니
+            <p>장바구니</p>
           </Link>
         </div>
-        <div className=" bg-orange-300 bottom-0">
+        <div className="bottom-0 grid grid-flow-col justify-stretch">
           <Link to="/">홈</Link>
           <Link to="signin">로그인</Link>
           <Link to="signup">회원가입</Link>
