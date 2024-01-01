0eaa09249009c810b5197e551bb9b77cd25359b0
parkjian7 <masichyun7@inswave.com>
2024-01-02 01:24:45 +0900

    home파일에서 tab-menu 구현

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -17,18 +17,33 @@ export default function Product({
   },
 }) {
   const navigate = useNavigate();
-  const handleClick = () => {
-    navigate(`/products/${id}`, { state: { item } });
+  const handleClick = (e) => {
+    if (item.quantity > 0) {
+      navigate(`/products/${id}`, { state: { item } });
+    } else {
+      e.preventDefault();
+    }
   };
   return (
     <li
-      onClick={handleClick}
-      className="rounded-lg shadow-md overflow-hidden cursor-pointer transition-all hover:scale-105"
+      onClick={(e) => handleClick(e)}
+      className="rounded-lg shadow-md overflow-hidden cursor-pointer transition-all hover:scale-105 list-none"
     >
-      <img className="w-full" id={id} src={thumbnails.default.url} alt={alt} />
+      <div className="inline-block relative">
+        <img
+          className={`w-full ${quantity <= 0 ? "opacity-50" : ""}`}
+          id={id}
+          src={thumbnails.default.url}
+          alt={alt}
+        />
+        {quantity <= 0 && (
+          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 py-1 px-2 rounded font-bold text-2xl">
+            품절
+          </div>
+        )}
+      </div>
       <div className="mt-2 px-2 text-lg flex justify-between items-center">
         <p>{`${name}`}</p>
-        <p>{`${quantity}개`}</p>
         <p>{`1박 ${price}원`}</p>
       </div>
     </li>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,26 @@
 import React, { useState, useEffect } from "react";
+import TabMenu from "../components/ui/TabMenu.jsx";
 import itemList from "../data/itemList.json";
-import Product from "../components/product/Product";
 
 export default function Home() {
-  const [product, setProduct] = useState([]);
+  const [selectedItemList, setSelectedItemList] = useState(
+    itemList.dailyBestItems
+  );
+  const handleTabChange = (index) => {
+    // 해당 index에 해당하는 itemList를 선택하여 TabMenu로 전달
+    setSelectedItemList(() => {
+      switch (index) {
+        case 0:
+          return itemList.dailyBestItems;
+        case 1:
+          return itemList.weeklyBestItems;
+        case 2:
+          return itemList.monthlyBestItems;
+        default:
+          return [];
+      }
+    });
+  };
   // const fetchProductImages = async () => {
   //   try {
   //     // 상품 이미지 목록을 가져오는 API 호출 또는 이미지 URL 배열을 가져오는 로직
@@ -17,31 +34,6 @@ export default function Home() {
   //     console.error('상품 이미지를 가져오는 중 오류가 발생했습니다.', error);
   //   }
   // };
-  const extractItem = (items) => {
-    // JSON 파일에서 이미지 URL 추출
-    const item = items.map((item) => {
-      return item;
-    });
-    setProduct(item);
-  };
 
-  useEffect(() => {
-    // 컴포넌트가 마운트될 때 상품 이미지를 가져오는 함수 호출
-    extractItem(itemList.items);
-  }, []); // 빈 배열을 전달하여 한 번만 호출되도록 설정
-  return (
-    <div className="bg-slate-200">
-      <h1>실시간 BEST</h1>
-      <ul className="flex">
-        <li>일간 BEST</li>
-        <li>주간 BEST</li>
-        <li>월간 BEST</li>
-      </ul>
-      <div className="grid grid-cols-1 md:grid-cols-3 lg-grid-cols-4 gap-4 p-4">
-        {product.map((item, index) => (
-          <Product key={item.id} alt={`상품 이미지 ${index + 1}`} item={item} />
-        ))}
-      </div>
-    </div>
-  );
+  return <TabMenu onTabChange={handleTabChange} itemList={selectedItemList} />;
 }
